ccbe5d31244b9fa27f8c984084a3ac681287afc2
Prashant Solanki <ps.magpapifan@gmail.com>
2026-07-01 15:50:21 +0530

    Initial Commit

---
+++ ScopeCompanion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScopeCompanion
+//
+//  Created by Mac412 on 01/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScopeCompanion/ScopeCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScopeCompanionApp.swift
+//  ScopeCompanion
+//
+//  Created by Mac412 on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ScopeCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
